15abb718647ebd4c899dd8af4351d9b786bac683
John <153831049+Smoldersob@users.noreply.github.com>
2026-02-09 10:32:29 +0100

    Update __init__.py
    Adding parent class to lower layer of library

--- complex_granger_analysis/__init__.py
+++ complex_granger_analysis/__init__.py
@@ -1,4 +1,5 @@
 import importlib
+from .granger_tests.complex_granger import ComplexGrangerAnalisysModel
 from .granger_tests.scikit_granger import SparseConstaraintedMVGC, MTCLR
 from .granger_tests.statsmodels_granger import grangers_causation_matrix
 from .callbacks.callbacks import Callback,ProcentageChange,EarlyStopping
@@ -6,7 +7,7 @@ from .callbacks.callbacks import Callback,ProcentageChange,EarlyStopping
 if importlib.util.find_spec("statsmodels") is None:
     raise ImportError("statsmodels is required")
 
-__all__ = ['grangers_causation_matrix', 'Callback','ProcentageChange', 'EarlyStopping','auto_select_lag']
+__all__ = ['ComplexGrangerAnalisysModel','grangers_causation_matrix', 'Callback','ProcentageChange', 'EarlyStopping','auto_select_lag']
 
 if importlib.util.find_spec("sklearn") is not None:
     __all__.extend(['SparseConstaraintedMVGC','MTCLR'])
